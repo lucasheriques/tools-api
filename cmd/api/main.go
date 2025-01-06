dda24b050babaf448aead49b39841ea4096b9d58
Lucas Faria <lucasheriques@gmail.com>
2025-01-05 23:52:55 -0300

    with rate limiter

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -12,8 +12,13 @@ import (
 const version = "1.0.0"
 
 type config struct {
-	port int
-	env  string
+	port    int
+	env     string
+	limiter struct {
+		rps     float64
+		burst   int
+		enabled bool
+	}
 }
 
 type application struct {
@@ -26,6 +31,9 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2, "Rate limiter maximum requests per second")
+	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
+	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
